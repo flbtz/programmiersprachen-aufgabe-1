14ccfcc239c8b55d247e386cc28b7b1fd3d7acbe
Florian B <florian_batze@gmx.de>
2018-04-27 14:45:34 +0200

    Aufgabe1_3.cpp
    Aufgabe 1.3 hinzugefügt

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,6 +51,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "source/CMakeFiles/aufgabe1_3.dir/DependInfo.cmake"
   "source/CMakeFiles/tests.dir/DependInfo.cmake"
   "source/CMakeFiles/helloworld.dir/DependInfo.cmake"
   )
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,36 @@
-#define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+# define CATCH_CONFIG_RUNNER
+# include "catch.hpp"
+# include <cmath>
+#include <string>
 
-int main(int argc, char* argv[])
+int _ergebnis = 0;
+int _rest = 0;
+int _temp = 0;
+
+int gcd(int const& a, int const& b)
 {
-  return Catch::Session().run(argc, argv);
+    if(a>b){
+        _ergebnis = b;
+        _rest = a%b;
+    }
+    else if(a<b){
+        _ergebnis = a;
+        _rest = b%a;
+    }
+
+    if(_rest == 0){
+        return _ergebnis;
+    }
+    return gcd(b,_rest);
+}
+
+TEST_CASE ("describe_gcd", "[gcd]")
+{
+REQUIRE (gcd (2 ,4) == 2);
+REQUIRE (gcd (9 ,6) == 3);
+REQUIRE (gcd (3 ,7) == 1);
+}
+int main (int argc , char * argv [])
+{
+    return Catch :: Session (). run( argc , argv );
 }
